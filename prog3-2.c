5b8b5f0dda7cea18e23274ea6ecaa4dbd743bb2b
DevanshGanatra <140813128+DevanshGanatra@users.noreply.github.com>
2023-08-20 17:56:59 +0530

    Add files via upload

---
+++ prog3-2.c
@@ -0,0 +1,12 @@
+// addition of 2 no. with using scanf
+#include<stdio.h>
+void main()
+{
+	int a,b,c;
+	printf("Enter an integer value: ");
+	scanf("%d",&a);
+	printf("Enter an ineger value: ");
+	scanf("%d",&b);
+	c=a+b;
+	printf("Sum is=%d",c);
+}
