d0c6dce6f010a4400bd7faf15594323eee48b255
DevanshGanatra <140813128+DevanshGanatra@users.noreply.github.com>
2023-08-21 09:24:50 +0530

    Add files via upload

---
+++ prog5.c
@@ -0,0 +1,10 @@
+//print area of circle
+#include<stdio.h>
+void main()
+{
+	float r,area;
+	printf("enter radius in integer ");
+	scanf("%f",&r);
+	area=r*r*3.14;
+	printf("area of circle=%f",area);
+}
