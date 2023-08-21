f55713ae885ff8e0ca7488cf3689e590908efe42
DevanshGanatra <140813128+DevanshGanatra@users.noreply.github.com>
2023-08-21 09:28:02 +0530

    Add files via upload

---
+++ prog9.c
@@ -0,0 +1,10 @@
+//convert feet into inches
+#include<stdio.h>
+void main()
+{
+	float feet,inches;
+	printf("enter feet ");
+	scanf("%f",&feet);
+	inches=feet*12;
+	printf("feet in inches would be =%f",inches);
+}
