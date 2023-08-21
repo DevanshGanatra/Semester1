fbb6490cd0ddee712524fd18d9ad0d75e005dff1
DevanshGanatra <140813128+DevanshGanatra@users.noreply.github.com>
2023-08-21 09:25:17 +0530

    Add files via upload

---
+++ prog6.c
@@ -0,0 +1,12 @@
+// print area of triangle 
+#include<stdio.h>
+void main()
+{
+	float h,b,area;
+	printf("enter height of triangle ");
+	scanf("%f",&h);
+	printf("enter base of triangle ");
+	scanf("%f",&b);
+	area=(h*b)/2.0;
+	printf("area of triangle=%f",area);
+}
