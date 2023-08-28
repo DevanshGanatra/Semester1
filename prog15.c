cab13ccb46b06551885b838de7dbe138c47c8dd6
DevanshGanatra <140813128+DevanshGanatra@users.noreply.github.com>
2023-08-28 09:13:55 +0530

    Add files via upload

---
+++ prog15.c
@@ -0,0 +1,36 @@
+//Perform Addition, Subtraction, Multiplication and Division of 2 numbers as per user’s choice. 
+#include<stdio.h>
+void main()
+{
+	int choice;
+	float div,a,b,add,sub,mul;
+	printf("Enter the value of A and B :");
+	scanf("%d %d",&a,&b);
+	printf("1.addition 2.subtraction 3.multiplication 4.division\n");
+	printf("enter your choice");
+	scanf("%d",&choice);
+	if (choice==1)
+	{
+		add=a+b;
+		printf("addition=%f",add);
+	}
+		else if (choice==2)
+	{
+		sub=a-b;
+		printf("subtraction=%f",sub);
+	}
+		else if (choice==3)
+	{
+		mul=a*b;
+		printf("multiplication=%f",mul);
+	}
+		else if (choice==4)
+	{
+		div=(a/b);
+		printf("division=%f",div);
+	}
+	else
+	{
+		printf("enter an valid choice");
+	}
+}
