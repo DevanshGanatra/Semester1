f888a194d5ec4da611f113a10d0ca6cd9a5c1731
DevanshGanatra <140813128+DevanshGanatra@users.noreply.github.com>
2023-08-28 09:27:57 +0530

    Add files via upload

---
+++ prog12.c
@@ -0,0 +1,16 @@
+// check no. is odd or even
+#include<stdio.h>
+void main()
+{
+	int n;
+	printf("enter an integer");
+	scanf("%d",&n);
+	if(n%2!=0)
+	{
+	printf("no is odd");
+	}
+	else
+	{
+	printf("no is even");
+	}
+}
