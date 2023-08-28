bed66b704d9792bdf4b7c6b5e854b0bdce8e6916
DevanshGanatra <140813128+DevanshGanatra@users.noreply.github.com>
2023-08-28 09:27:36 +0530

    Add files via upload

---
+++ prog11.c
@@ -0,0 +1,16 @@
+//check the no. is positive or negative
+#include<stdio.h>
+void main()
+{
+int no;
+printf("enter any integer except 0 ");
+scanf("%d",&no);
+if(no>0)
+{
+	printf("Entered no. is positive");
+	}	
+	else
+	{
+		printf("Entered no. is negative");
+	}	
+}
